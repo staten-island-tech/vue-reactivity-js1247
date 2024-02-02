fbb4d089a68e42ced4801d408b03814a8cac98ab
js1247 <jasonshi1247@gmail.com>
2024-02-02 09:25:06 -0500

    925

---
+++ vue-project/src/components/WelcomeItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 v-if="test">Good</h1>
+    <h1 v-else>bad</h1>
+    <h2></h2>
+  </div>
+</template>
+
+<script setup>
+const animals = ["zebra", "camel", "ape"];
+const test =false;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
