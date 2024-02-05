424fcceee4de968fb782c4deeed7c46f279db2ae
js1247 <jasonshi1247@gmail.com>
2024-02-05 09:24:24 -0500

    924

---
+++ vue-project/src/components/DestCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ Destination.name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  Destination: Object,
+});
+</script>
+
+<style lang="scss" scoped></style>
