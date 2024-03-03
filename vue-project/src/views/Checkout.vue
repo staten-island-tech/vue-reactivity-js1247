4299cd5605daec44edda42b2a71efa607207d573
js1247 <jasonshi1247@gmail.com>
2024-03-03 17:12:29 -0500

    512

--- vue-project/src/views/Checkout.vue
+++ vue-project/src/views/Checkout.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="checkout">
-    <h1>This is the checkout page</h1>
-    <ShoppingCart
-      v-for="item in snacks"
-      :name="item.name"
-      :type="item.type"
-      :price="item.price"
-      :image="item.image"
-    ></ShoppingCart>
+  <div>
+    <h1>Checkout</h1>
+    
+    <div v-if="cart.length === 0">Your cart is empty</div>
+    <div v-else>
+      <ul>
+        <li v-for="(item, index) in cart" :key="index">
+          {{ item.name }} - ${{ item.price }}
+        </li>
+      </ul>
+      <div>Total: ${{ calculateTotalPrice() }}</div>
+      <button @click="completePurchase" class="complete-purchase">Complete Purchase</button>
+    </div>
   </div>
 </template>
-<script setup>
 
+<script setup>
+import { ref } from 'vue';
+import { store } from "@/components/store.js";
+
+const cart = ref(store.cart);
+
+function calculateTotalPrice() {
+  return cart.value.reduce((total, item) => total + item.price, 0);
+}
+
+function completePurchase() {
+  console.log('Purchase completed!');
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.complete-purchase {
+  margin-top: 10px;
+  cursor: pointer;
+  background-color: #64a166;
+  color: white;
+  border: none;
+  padding: 10px;
+  width: 100%;
+  transition: background-color 0.3s ease-in-out;
+  font-size: 2rem;
+}
+
+.complete-purchase:hover {
+  background-color: #4e8c5a;
+}
+</style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,25 +1,34 @@
 <template>
-  <div class="card">
-    <SnackCard
-      v-for="snack in snacks"
-      :name="snack.name"
-      :type="snack.type"
-      :price="snack.price"
-      :image="snack.image"
-    ></SnackCard>
+  <div>
+    <h1>Welcome to the Snack Shop!</h1>
+    <SnackCard />
+    <router-link to="/checkout" class="checkout-btn">Go to Checkout</router-link>
   </div>
 </template>
 
-<script setup>
-import { snacks } from "@/stores/array.js";
-import SnackCard from "@/components/SnackCard.vue"
+<script>
+import SnackCard from "@/components/SnackCard.vue";
+
+export default {
+  components: {
+    SnackCard,
+  },
+};
 </script>
 
 <style scoped>
-.card {
-  flex-direction: row;
-  justify-content: space-evenly;
-  display: flex;
-  flex-wrap: wrap;
+.checkout-btn {
+  margin-top: 20px;
+  display: inline-block;
+  padding: 10px;
+  background-color: #64a166;
+  color: white;
+  text-decoration: none;
+  font-size: 1.5rem;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.checkout-btn:hover {
+  background-color: #4e8c5a;
 }
 </style>
